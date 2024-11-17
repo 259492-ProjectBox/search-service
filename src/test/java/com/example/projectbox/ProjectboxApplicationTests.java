c307e62888913d9b5ec1b67fd20a561a5771a501
nono <nonokub.671@gmail.com>
2024-11-17 10:15:58 +0700

    init project

---
+++ src/test/java/com/example/projectbox/ProjectboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
